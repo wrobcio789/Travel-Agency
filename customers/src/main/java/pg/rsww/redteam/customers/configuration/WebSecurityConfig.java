3b0ce00897e424c76bed4bfcaf6fbf62e97efefc
Maciej <wrobcio789@gmail.com>
2022-05-08 06:33:52 +0200

    Fix formatting

--- customers/src/main/java/pg/rsww/redteam/customers/configuration/WebSecurityConfig.java
+++ customers/src/main/java/pg/rsww/redteam/customers/configuration/WebSecurityConfig.java
@@ -4,18 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import pg.rsww.redteam.customers.filters.AuthenticationFilter;
 import pg.rsww.redteam.customers.services.AppUserService;
-import pg.rsww.redteam.customers.services.AuthenticationService;
 
 @Configuration
 @EnableWebSecurity
