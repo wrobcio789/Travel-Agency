e07f23a899b8d2b2b6b5481ee2bfea6323fed796
hubicios <hubicios@protonmail.com>
2022-06-09 14:45:41 +0200

    TourChange send list of search response

--- services/src/Pg.Rsww.RedTeam.OfferService.Api/Models/TourSearchResponse.cs
+++ services/src/Pg.Rsww.RedTeam.OfferService.Api/Models/TourSearchResponse.cs
@@ -7,4 +7,5 @@ public class TourSearchResponse
 	public string Arrival { get; set; }
 	public DateTime StartDate { get; set; }
 	public DateTime EndDate { get; set; }
+	public bool Enabled { get; set; }
 }
